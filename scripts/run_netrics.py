4b1bd37a82651db8763d9b02bfb1b7c1b949568c
shanksc <shanksc@oregonstate.edu>
2023-04-10 22:00:55 +0000

    added .gitignore, added logs to run_netrics.py

--- scripts/run_netrics.py
+++ scripts/run_netrics.py
@@ -2,9 +2,12 @@ from gcp_upload import upload_blob, list_buckets
 import argparse
 import subprocess
 from socket import gethostname
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 from apscheduler.schedulers.blocking import BlockingScheduler
 import pytz
+import logging
+import os
+import sys
 
 
 TZ_STR='US/Pacific'
@@ -17,6 +20,7 @@ def get_args():
     parser.add_argument('--start', help='starting date in PST TZ and ISO 8601 format (yyyy-mm-ddTHH:MM:SS)', required=True)
     parser.add_argument('--interval', type=int, help='interval in minutes', required=False, default=5)
     parser.add_argument('--n', type=int, help='number of tests, do not exceed 40 in 24hrs', required=False, default=10)    
+    parser.add_argument('--logs', type=str, help='directory for logs', required=True)    
 
     return parser.parse_args()
 
@@ -27,10 +31,9 @@ def run_test(test_name, test_arg, args):
         subprocess.run(['netrics', test_arg], stdout=f)
     
     fname = '{0}-{1}-{2}.txt'.format(test_name, gethostname(), datetime.now(tz=pytz.timezone(TZ_STR)).isoformat())
-    print(fname)
  
     upload_blob(args.bucket_name, test_name+'.tmp', fname, args.service_account)
-    print('uploaded {0}'.format(fname))
+    logging.info('uploaded {0}'.format(fname))
     
     subprocess.run(['rm', test_name+'.tmp'])
 
@@ -40,21 +43,37 @@ def all_tests(args):
     run_test('ookla', '-k', args)
     run_test('ndt7', '-a', args)
     
-    print('ran test')
+    #print('ran test')
 
 if __name__ == '__main__':
     print('running netrics')
     args = get_args()    
     
-    
     #Timezone naive solution
     run_date = datetime.fromisoformat(args.start)
-    #-8:00 hours offset for PST from UTC
-    now = datetime.now() - timedelta(hours=8)
+    #-7:00 hours offset for PST from UTC
+    now = datetime.now() - timedelta(hours=7)
     
+ 
     #check that start is in the future
-    if not run_date > now:
+    if run_date < now:
         raise ValueError('entered start time already past.')
+    
+    path = args.logs
+    if os.path.exists(path):
+        print('logging files to ' + path)
+    else:
+        print('invalid path for logging')
+        sys.exit()
+ 
+    filename=args.logs+(run_date.isoformat())+'.log'
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
+    logging.basicConfig(filename=args.logs+(run_date.isoformat())+'.log', encoding='utf-8', level=logging.INFO)
+    logging.info('Command: ' + ' '.join(sys.argv))    
     
     inc = timedelta(minutes=args.interval)
     
@@ -62,11 +81,21 @@ if __name__ == '__main__':
 
     for n in range(args.n):
         sched.add_job(all_tests, 'date', run_date=run_date, timezone=TZ_STR, args=[args])
+        if n == args.n-1:
+            run_time = run_date.strftime("%H:%M:%S")
+            logging.info('Last speedtest to be ran at : ' + run_time) 
+
         #increment start time by inc
         run_date += inc
     
-    sched.print_jobs()    
+    
+
+    #try:
     sched.start()
-
-
+    #except:
+    #    print('Error starting schedule')
+    #    sys.exit()
+    
+    
+    
 
