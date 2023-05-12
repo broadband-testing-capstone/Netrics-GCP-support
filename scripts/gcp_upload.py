3b1e9fa7dbded21128037b26ee73e24ce7b6e2c1
shanksc <shanksc@oregonstate.edu>
2023-05-12 09:07:33 -0700

    Update gcp_upload.py

--- scripts/gcp_upload.py
+++ scripts/gcp_upload.py
@@ -13,8 +13,6 @@ def upload_blob(bucket_name, file_name, blob_name, service_account_file):
 
     blob.upload_from_filename(file_name)
 
-    #print(f"File {file_name} uploaded to {blob_name}.")
-
 
 def get_bucket(service_account_file, bucket):
     storage_client = storage.Client.from_service_account_json(service_account_file)
